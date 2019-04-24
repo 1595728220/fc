90d471c8ada942b78756e9debfb7efa755c33d6d
1595728220 <l>
2019-04-24 23:54:10 +0800

    config my project

--- fc_front/src/main.js
+++ fc_front/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import axios from "axios"
+Vue.prototype.$ajax = axios
+//css
+import "./assets/css/bootstrap.css"
+import "./assets/css/font-awesome.css"
+import "./assets/css/animate.css"
+import "./assets/css/app.css"
+//js
+import "./assets/js/jquery"
+import "./assets/js/jquery.flot"
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
