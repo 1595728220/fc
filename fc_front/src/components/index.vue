2ce9b722b1be08394a6eb6d81cff2c1f58dcce6b
1595728220 <1595728220@qq.com>
2019-04-26 09:00:19 +0800

    some warning from import bootstrap

--- fc_front/src/components/index.vue
+++ fc_front/src/components/index.vue
@@ -6,18 +6,17 @@
         </div>
         <myfooter></myfooter>
     </div>
-    
+
 </template>
 <script>
-export default {
-    data(){
-        return {
-            msg:"hello world"
+    export default {
+        data() {
+            return {
+                msg: "hello world"
+            }
         }
     }
-}
 </script>
 <style>
 
 </style>
-
