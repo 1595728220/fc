d96a03f80320f5276781f42cc67c713d7892c7f0
1595728220 <1595728220@qq.com>
2019-05-05 20:57:28 +0800

    can't load img

--- fc_front/src/components/index.vue
+++ fc_front/src/components/index.vue
@@ -6,7 +6,13 @@
             <div class="banner bg-secondary row m-0">
                 <div class="col-lg-10 col-sm-12"></div>
                 <div class="col-lg-2 col-sm-12 d-flex flex-sm-row flex-lg-column">
-
+                    <ul>
+                        <li v-for="(val,key,index) in banner.imgMsg" :key="index">
+                            <router-link :to="'/detail/'+val.pid">
+                                <img :src="val.src">
+                            </router-link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -18,11 +24,27 @@
     export default {
         data() {
             return {
-                banner:{},
+                banner:{
+                    baseAddr:"../assets/imgs/banner/",
+                    imgMsg:{},
+                },
             }
         },
         mounted() {
-            this.$axios.get("/")
+            this.$axios.get("/product/banner").then(result=>{
+                this.banner.imgMsg = result.data
+                this.banner.imgMsg.forEach(el=>{
+                    el.src = require(this.banner.baseAddr+el.banner)
+                    // el.src=require("../assets/imgs/banner/banner1.jpg")
+                    // el.test = require("../assets/imgs/banner/banner1.jpg")
+                })
+                console.log(this.banner.imgMsg[0].src)
+            }).catch(error=>{
+                throw error
+            })
+        },
+        computed: {
+            
         },
     }
 </script>
