095b4c75254584b031969c4c9b26a13450a53b91
1595728220 <1595728220@qq.com>
2019-05-14 13:41:43 +0800

    appdownload finish

--- fc_front/src/components/appdown.vue
+++ fc_front/src/components/appdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="down_area text-center mb-5">
-      <img src="../assets/imgs/header/logo.png" class="mt-3">
+      <img src="../assets/imgs/header/logo.png" class="mt-5">
       <p class="mt-3 text-center">对庄翡翠，100万用户都在</p>
-      <img src="../assets/imgs/download/downapp.png" class="mb-3">
+      <img src="../assets/imgs/download/downapp.png" class="mb-5">
     </div>
-    
+    <mytuwen :mymsg="tuwen"></mytuwen>  
   </div>
 </template>
 <script>
@@ -14,31 +14,31 @@ export default {
     return {
       tuwen: [
         {
-          tu: require("../assets/imgs/download/550.jpg"),
+          tu: "550.jpg",
           wen_title: "文案文案文案文案文案文案",
           wen_detail:
             "我是文案我是文案我是文案我是文案我是文案我是文案我是文案我是文案"
         },
         {
-          tu: require("../assets/imgs/download/551.jpg"),
+          tu: "551.jpg",
           wen_title: "文案文案文案文案文案文案",
           wen_detail:
             "我是文案我是文案我是文案我是文案我是文案我是文案我是文案我是文案"
         },
         {
-          tu: require("../assets/imgs/download/552.jpg"),
+          tu: "552.jpg",
           wen_title: "文案文案文案文案文案文案",
           wen_detail:
             "我是文案我是文案我是文案我是文案我是文案我是文案我是文案我是文案"
         },
         {
-          tu: require("../assets/imgs/download/553.jpg"),
+          tu: "553.jpg",
           wen_title: "文案文案文案文案文案文案",
           wen_detail:
             "我是文案我是文案我是文案我是文案我是文案我是文案我是文案我是文案"
         },
         {
-          tu: require("../assets/imgs/download/554.jpg"),
+          tu: "554.jpg",
           wen_title: "文案文案文案文案文案文案",
           wen_detail:
             "我是文案我是文案我是文案我是文案我是文案我是文案我是文案我是文案"
--- fc_front/src/components/mytuwen.vue
+++ fc_front/src/components/mytuwen.vue
@@ -1,17 +1,48 @@
 <template>
-  
+  <div class="d-flex flex-column">
+    <div v-for="(obj,index) in mymsg" :key="index" class="w-100 pt-5 pb-5 d-flex justify-content-around" :class="{gray_bg:index%2 === 1}">
+      <!-- <img :src="imgSrc(obj.tu)" class="tu" :class="{fr:index%2 === 1,fl:index%2 === 0}"> -->
+      <img :src="imgSrc(obj.tu)">
+      <div>
+        <p class="zi">{{obj.wen_title}}</p>
+        <p class="zi">{{obj.wen_detail}}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  prop:["mymsg"],
-  data(){
-    return{
-      
+  props: ["mymsg"],
+  data() {
+    return {};
+  },
+  mounted() {
+    // console.log(this.mymsg);
+  },
+  computed: {},
+  methods: {
+    imgSrc(name) {
+      return require("../assets/imgs/download/" + name);
     }
   }
-}
+  // watch: {
+  //   mymsg(){
+  //     console.log(this.mymsg)
+  //   }
+  // },
+};
 </script>
 <style scoped>
 
+.gray_bg {
+  background: #ddd;
+  justify-content: space-around;
+  flex-direction: row-reverse
+}
+.zi:first-child{
+  font-size:1.875rem;
+  font-weight: bold;
+  margin-top:3.125rem;
+}
 </style>
 
