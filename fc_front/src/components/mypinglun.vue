3b998ed1d6deaec00effa05a351afa45b98c9ea3
1595728220 <1595728220@qq.com>
2019-05-15 20:24:52 +0800

    mypinglun finish

--- fc_front/src/components/mypinglun.vue
+++ fc_front/src/components/mypinglun.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="mt-3">
+  <div class="">
     <h2 v-if="!!nothing">{{nothing}}</h2>
-    <div class="w-100" v-for="(obj,index) in words" :key="index">{{obj.content}}</div>
+    <div class="w-100 user" v-for="(obj,index) in words" :key="index">
+        <img :src="'http:\/\/127.0.0.1:8081/user/'+obj.img_addr" class="fl mt-2">
+        <span>{{obj.nick}}</span>
+        <p>{{obj.content}}</p>
+    </div>
   </div>
 </template>
 <script>
@@ -10,7 +14,7 @@ export default {
   data() {
     return {
       words: [],
-      imgSrc : [],
+      imgSrc: [],
       nothing: ""
     };
   },
@@ -35,7 +39,7 @@ export default {
         .catch(err => {
           throw err;
         });
-    },
+    }
     // words(){
     //   if(words.length > 0) {
     //     for(let val of words){
@@ -47,4 +51,23 @@ export default {
 };
 </script>
 <style scoped>
+.user {
+  padding: 1.875rem;
+  border-top: 0.0625rem solid #ccc;
+  border-bottom: 0.0625rem solid #ccc;
+}
+.user img {
+  width: 2rem;
+  vertical-align: bottom;
+}
+.user span {
+  padding-left: 1.25rem;
+  font-size: 1rem;
+  color: #0aa1ed;
+}
+.user p {
+  padding-left: 3.25rem;
+  font-size: 1.25rem;
+  margin-top:.625rem;
+}
 </style>
