776376079997fe4923965f667e60dfb9a6cb9951
1595728220 <1595728220@qq.com>
2019-05-15 08:57:48 +0800

    detail top content

--- fc_front/src/components/detail.vue
+++ fc_front/src/components/detail.vue
@@ -1,22 +1,50 @@
 <template>
-  <div class="row m-0 detail bg-secondary">
+  <div class="row m-0 detail ">
     <div class="col-lg-7 tu">
       <div class="row m-0">
         <div class="col-lg-10">
           <img :src="imgSrc.product[showIndex]" alt class="w-100">
         </div>
         <div class="col-lg-2 d-flex flex-column justify-content-between">
-          <img :src="val" v-for="(val,index) in imgSrc.product" :key="index" class="w-100" @click="qiehuan(index)">
+          <img
+            :src="val"
+            v-for="(val,index) in imgSrc.product"
+            :key="index"
+            class="w-100"
+            @click="qiehuan(index)"
+          >
         </div>
       </div>
     </div>
-    <div class="col-lg-5 wen d-flex flex-column justify-content-between">
+    <div class="col-lg-5 wen d-flex flex-column justify-content-around">
       <!-- <div class=" "> -->
-        <h3>{{product.described}}</h3>
+      <div>
+        <h3 class="font-weight-bold">{{product.described}}</h3>
         <h5>货号:{{product.artNo}}</h5>
-        <h5>建议零售价:{{product.price}}</h5>
-        <h5>提示:该宝贝只能在对庄APP内购买</h5>
-        <router-link to="/appdown" class="btn btn-success">APP内购买</router-link>
+        <h5 class="text-danger">建议零售价:￥{{product.price}}</h5>
+      </div>
+      <div class="">
+        <h5 class="text-secondary">提示:该宝贝只能在对庄APP内购买</h5>
+        <router-link to="/appdown" class="btn btn-success w-100">APP内购买</router-link>
+      </div>
+        <div class="d-flex justify-content-around pt-5">
+        <div class="p-0">
+          <p class="index_icon promise"></p>
+          <span class="icon_text">翡翠保A承若</span>
+        </div>
+        <div class="p-0">
+          <p class="index_icon pifa"></p>
+          <span class="icon_text">源头批发</span>
+        </div>
+        <div class="p-0">
+          <p class="index_icon rushi"></p>
+          <span class="icon_text">手机如实拍摄</span>
+        </div>
+        <div class="p-0">
+          <p class="index_icon tuikuan"></p>
+          <span class="icon_text">7天无理由退款</span>
+        </div>
+      </div>
       <!-- </div> -->
     </div>
   </div>
@@ -27,14 +55,14 @@ export default {
     return {
       pid: 0,
       product: {},
-      imgSrc:{product:[],promise:[]},
-      showIndex:0
+      imgSrc: { product: [], promise: [] },
+      showIndex: 0
     };
   },
   methods: {
-    qiehuan(val){
+    qiehuan(val) {
       // console.log(val)
-      this.showIndex = val
+      this.showIndex = val;
     }
   },
   mounted() {
@@ -46,18 +74,22 @@ export default {
         params: { pid: this.pid }
       })
       .then(result => {
-        for (let key in result.data[0]){
+        for (let key in result.data[0]) {
           this.$set(this.product, key, result.data[0][key]);
-          switch(key){
+          switch (key) {
             case "photo1":
             case "photo2":
             case "photo3":
             case "photo4":
-              this.imgSrc.product.push(require('../assets/imgs/product/'+result.data[0][key]))
-              break
+              this.imgSrc.product.push(
+                require("../assets/imgs/product/" + result.data[0][key])
+              );
+              break;
             case "promise":
-              this.imgSrc.promise.push(require('../assets/imgs/product/'+result.data[0][key]))
-              break
+              this.imgSrc.promise.push(
+                require("../assets/imgs/product/" + result.data[0][key])
+              );
+              break;
             default:
           }
         }
@@ -75,4 +107,26 @@ export default {
 };
 </script>
 <style scoped>
+.wen>div:nth-child(3){
+  border-top:1px solid #ccc;
+}
+.index_icon {
+  width: 4.1rem;
+  height: 4.2rem;
+  background: url(../assets/imgs/icon/icons.png) -233px -125px;
+  background-size: 20rem;
+  margin: 0 auto;
+}
+.icon_text {
+  font-size: 20px;
+}
+.index_icon.pifa {
+  background-position: -341px -19px;
+}
+.index_icon.rushi {
+  background-position: -341px -125px;
+}
+.index_icon.tuikuan {
+  background-position: -127px -125px;
+}
 </style>
