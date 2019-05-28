d9fb7cd7e7d78a48f9e797725042afc24fa21ed3
1595728220 <1595728220@qq.com>
2019-05-28 20:43:42 +0800

    搭建移动端目录结构

--- fc_front_app/src/App.vue
+++ fc_front_app/src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <div id="app" class="container-fluid p-0" @click="click_hidden_dropdown">
-    <myheader></myheader>
+<div id="app" class="container-fluid p-0">
+  <!-- <div id="app" class="container-fluid p-0" @click="click_hidden_dropdown"> -->
+    <!-- <myheader></myheader> -->
     <router-view></router-view>
-    <myfooter></myfooter>
+    <!-- <myfooter></myfooter> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
-  methods: {
-    click_hidden_dropdown() {
-      if (this.$store.getters.get_class_is_show_dropdown)
-        this.$store.dispatch("set_class_is_show_dropdown", false);
-      if (this.$store.getters.get_search_is_show_dropdown)
-        this.$store.dispatch("set_search_is_show_dropdown", false);
-    }
-  }
+  // methods: {
+  //   click_hidden_dropdown() {
+  //     if (this.$store.getters.get_class_is_show_dropdown)
+  //       this.$store.dispatch("set_class_is_show_dropdown", false);
+  //     if (this.$store.getters.get_search_is_show_dropdown)
+  //       this.$store.dispatch("set_search_is_show_dropdown", false);
+  //   }
+  // }
 };
 </script>
 
