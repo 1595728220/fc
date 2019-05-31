dc3e096a7d9990fb065740c79f7ac9087985b14c
1595728220 <1595728220@qq.com>
2019-05-31 10:24:39 +0800

    修改vuex中查找产品参数和请求产品的方法

--- fc_front_app/src/store/index.js
+++ fc_front_app/src/store/index.js
@@ -10,20 +10,22 @@ const state = {
   phoneRegex: /^1[34578]\d{9}$/,
   //密码正则
   // upwdRegex: /^[a-zA-Z\d_]{6,18}$/,
+  //产品列表
   productList: [],
-  productQuery:{
-    keywords:"",
-    classify:"",
-    style:"",
-    thickness:"",
-    color:"",
-    minprice:"",
-    maxprice:"",
-    rexiao:false,
-    xinpin:false,
-    recommond:false,
-    pno:1,
-    size:6,
+  //查询产品的参数
+  productQuery: {
+    keywords: "",
+    classify: "",
+    style: "",
+    thickness: "",
+    color: "",
+    minprice: "",
+    maxprice: "",
+    rexiao: false,
+    xinpin: false,
+    recommond: false,
+    pno: 1,
+    size: 6,
   }
 };
 const getters = { //实时监听state值的变化(最新状态)
@@ -31,27 +33,41 @@ const getters = { //实时监听state值的变化(最新状态)
 };
 const mutations = {
   //自定义改变state初始值的方法，这里面的参数除了state之外还可以再传额外的参数(变量或对象);
+
   //修改产品列表数据
   setProductList(state, val) {
     state.productList = val
-  }
+  },
+  //初始化查询参数
+  initProductQuery(state) {
+    state.productQuery = {
+      keywords: "",
+      classify: "",
+      style: "",
+      thickness: "",
+      color: "",
+      minprice: "",
+      maxprice: "",
+      rexiao: false,
+      xinpin: false,
+      recommond: false,
+      pno: 1,
+      size: 6,
+    }
+  },
 };
 const actions = { //this.$store.dispatch('set_uid'，6)
   //自定义触发mutations里函数的方法，context与store 实例具有相同方法和属性  
-  getProductList(context, keywords) {
-    // if (keywords.length === 0) {
-    //   console.log("keywords为空")
-    //   return
-    // }
+  //发送请求，查询符合条件的产品信息
+  getProductList(context) {
+    console.log(state.productQuery)
     axios.get("/product/list", {
-      params: {
-        keywords
-      }
+      params: state.productQuery
     }).then(result => {
       console.log(result.data[0])
-      context.commit("setProductList",result.data[0])
+      context.commit("setProductList", result.data[0])
     }).catch(err => { throw err })
-  }
+  },
 };
 const store = new Vuex.Store({
   state,
