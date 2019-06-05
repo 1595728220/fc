3857172896c3734e42a71c379aa62c3329d9e4cf
1595728220 <1595728220@qq.com>
2019-06-05 17:51:38 +0800

    在详情页的收藏组件中无法更新单个产品的信息，于是使用垃圾方法，获取产品总列表，取出对应Id的一项,再传给收藏组件

--- fc_front_app/src/components/common/immediatelyBuy.vue
+++ fc_front_app/src/components/common/immediatelyBuy.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="immediately-buy">
-    
+    <my-collect :productItem="productItem"></my-collect>
   </div>
 </template>
 <script>
-import Collect from "../common/collect"
+import Collect from "../common/collect";
 export default {
-  props:{
-    pid:{default:""}
+  props: {
+    pid: { default: "" }
   },
   data() {
-    return {
-      
+    return {};
+  },
+  components: {
+    "my-collect": Collect
+  },
+  computed: {
+    productItem() {
+      return this.$store.state.productList.filter(el => el.pid == this.pid)[0];
     }
   },
-  components:{
-    "my-collect":Collect
+  mounted() {
+    this.$store.dispatch("getProductList");
+    console.log(this.productItem)
   },
-}
+  methods: {}
+};
 </script>
 <style lang="scss" scoped>
-  .immediately-buy{
-    position: fixed;
-    bottom: 0;
-    background:#fff;
-    width:100%;
-  }
+.immediately-buy {
+  position: fixed;
+  bottom: 0;
+  background: #fff;
+  width: 100%;
+}
 </style>
