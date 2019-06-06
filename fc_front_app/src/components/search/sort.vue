250e488530e5a246df78b9f105c091f88781ec19
1595728220 <1595728220@qq.com>
2019-06-06 10:50:08 +0800

    产品分类项完成

--- fc_front_app/src/components/search/sort.vue
+++ fc_front_app/src/components/search/sort.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="sort">
     <mt-navbar v-model="selected" class="mynavbar">
-      <mt-tab-item :id="name" v-for="(arr,name,index) of product_classfy" :key="index">{{name}}</mt-tab-item>
+      <mt-tab-item :id="name" v-for="(arr,name,index) of product_classfy" :key="index">{{name|fanyi}}</mt-tab-item>
     </mt-navbar>
     <mt-tab-container class="page-tabbar-container" v-model="selected">
-      <mt-tab-container-item :id="name" v-for="(arr,name,index) of product_classfy" :key="index">{{arr}}</mt-tab-container-item>
+      <mt-tab-container-item :id="name" v-for="(arr,name,index) of product_classfy" :key="index" class="sort-list">
+        <sort-item v-for="(val,ind) of arr" :key="ind" :num="''+index+ind" :sort="outObject(name,val)"></sort-item>
+      </mt-tab-container-item>
     </mt-tab-container>
   </div>
 </template>
 <script>
+import SortItem from "./sort_item"
 export default {
   data() {
     return {
       //产品的分类情况
       product_classfy: {
-        颜色: [],
-        种水: []
+
       },
-      selected: "颜色"
+      selected: "classify"
     };
   },
   created() {
@@ -30,21 +32,24 @@ export default {
         color: [],
         thickness: []
       };
+      // console.log(result.data)
       //遍历结果消息的数组
       for (let row of result.data) {
         //如果存在分类则新增分类下的样式，如果不存在分类，则创建分类，并添加样式
-        this.product_classfy[row.classify]
-          ? this.check_array(this.product_classfy[row.classify], row.style)
-          : (this.product_classfy[row.classify] = [row.style]);
-        //往产品分类对象的颜色属性所对应的数组中插入原本没有的值
-        this.check_array(this.product_classfy["颜色"], row.color);
-        //往产品分类对象的种水属性所对应的数组中插入原本没有的值
-        this.check_array(this.product_classfy["种水"], row.thickness);
+        // this.product_classfy[row.classify]
+        //   ? this.check_array(this.product_classfy[row.classify], row.style)
+        //   : (this.product_classfy[row.classify] = [row.style]);
+        // //往产品分类对象的颜色属性所对应的数组中插入原本没有的值
+        // this.check_array(this.product_classfy["颜色"], row.color);
+        // //往产品分类对象的种水属性所对应的数组中插入原本没有的值
+        // this.check_array(this.product_classfy["种水"], row.thickness);
         this.check_array(tmpArr.classify, row.classify);
         this.check_array(tmpArr.style, row.style);
         this.check_array(tmpArr.color, row.color);
         this.check_array(tmpArr.thickness, row.thickness);
       }
+      this.product_classfy = tmpArr
+      console.log(tmpArr)
     });
     console.log(this.product_classfy);
   },
@@ -56,7 +61,16 @@ export default {
         arr.push(el);
       }
       return arr;
+    },
+    //生成一个对象
+    outObject(name,val){
+      let tmp = {}
+      tmp[name] = val
+      return tmp
     }
+  },
+  components:{
+    "sort-item":SortItem
   }
 };
 </script>
@@ -85,6 +99,12 @@ export default {
   .page-tabbar-container{
     height:100vh;
     background-color:#f3f3f3;
+  }
+  .sort-list{
+    display:flex;
+    flex-wrap: wrap;
+    padding:1rem 1rem;
+    justify-content: flex-start;
   }
 }
 </style>
---
+++ fc_front_app/src/components/search/sort_item.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="sort-item" @click="searchProduct">
+    <div :class="`bg-${num}`"></div>
+    <div>{{Object.values(sort)[0]}}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    sort: { default: ""},
+    num: { default: "" }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    //查找对应的产品
+    searchProduct(){
+      //初始化查询参数
+      this.$store.commit("initProductQuery")
+      //修改查询参数
+      this.$store.commit("changeProductQuery",this.sort)
+      //跳转产品页
+      this.$router.push("/product")
+    }
+  },
+  mounted() {
+    console.log(this.sort)
+    // Object.values(JSON.parse(sort))[0]
+  },
+};
+</script>
+<style lang="scss" scoped>
+.sort-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 60px;
+  text-align: center;
+  margin-right:1rem;
+  margin-bottom:1rem;
+  .bg-20 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -10px -10px;
+  }
+
+  .bg-21 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -90px -10px;
+  }
+
+  .bg-22 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -10px -90px;
+  }
+
+  .bg-23 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -90px -90px;
+  }
+
+  .bg-24 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -170px -10px;
+  }
+
+  .bg-31 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -170px -90px;
+  }
+  .bg-30 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -170px -10px;
+  }
+
+  .bg-01 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -170px -90px;
+  }
+
+  .bg-10 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -10px -170px;
+  }
+
+  .bg-11 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -90px -170px;
+  }
+
+  .bg-12 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -170px -170px;
+  }
+
+  .bg-13 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -250px -10px;
+  }
+
+  .bg-00 {
+    width: 60px;
+    height: 60px;
+    background: url(/imgs/sort/css_sprites.png) -250px -90px;
+  }
+}
+</style>
