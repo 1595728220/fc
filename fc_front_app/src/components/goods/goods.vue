eb43b290977b37ac73a569c675ad8fe0f6fe6897
1595728220 <1595728220@qq.com>
2019-06-06 13:31:00 +0800

    在写我的宝贝面板

--- fc_front_app/src/components/goods/goods.vue
+++ fc_front_app/src/components/goods/goods.vue
@@ -1,18 +1,40 @@
 <template>
-  <div>
-    <h1>我的宝贝面板</h1>
+  <div class="goods">
+    <div>
+      <mt-navbar v-model="selected">
+        <mt-tab-item id="glance">浏览过的</mt-tab-item>
+        <mt-tab-item id="collect">我的收藏</mt-tab-item>
+      </mt-navbar>
+    </div>
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="glance">1</mt-tab-container-item>
+      <mt-tab-container-item id="collect">1</mt-tab-container-item>
+    </mt-tab-container>
   </div>
 </template>
 <script>
-
 export default {
   data() {
     return {
-      
-    }
-  },
-}
+      selected: "glance"
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
-
+.goods {
+  .mint-navbar .mint-tab-item {
+    padding: 0.5rem 0;
+    &:first-child {
+      border-right: 1px solid #ddd;
+    }
+    &.is-selected {
+      color: #00c17b;
+      border-bottom-color: #00c17b;
+    }
+    /deep/ .mint-tab-item-label {
+      font-size: 1.2rem;
+    }
+  }
+}
 </style>
--- fc_front_app/src/components/market/market.vue
+++ fc_front_app/src/components/market/market.vue
@@ -2,51 +2,54 @@
   <div>
     <div>
       <mt-navbar v-model="selected" fixed class="mynavbar">
-        <mt-tab-item id="新品" >新品</mt-tab-item>
-        <mt-tab-item id="热销" >热销</mt-tab-item>
+        <mt-tab-item id="新品">新品</mt-tab-item>
+        <mt-tab-item id="热销">热销</mt-tab-item>
       </mt-navbar>
     </div>
     <mt-tab-container v-model="selected">
-      <mt-tab-container-item id="新品" >
+      <mt-tab-container-item id="新品">
         <market-list :query="xinpin" v-if="selected === '新品'" origin="新品"></market-list>
       </mt-tab-container-item>
-      <mt-tab-container-item id="热销" >
+      <mt-tab-container-item id="热销">
         <market-list :query="rexiao" v-if="selected === '热销'" origin="热销"></market-list>
       </mt-tab-container-item>
     </mt-tab-container>
   </div>
 </template>
 <script>
-import marketList from "./market_list"
+import marketList from "./market_list";
 // import marketNew from "./market_new"
 export default {
   data() {
     return {
       selected: "新品",
-      rexiao:{rexiao:true},
-      xinpin:{xinpin:true}
+      rexiao: { rexiao: true },
+      xinpin: { xinpin: true }
     };
   },
-  components:{
-    "market-list":marketList,
-  },
+  components: {
+    "market-list": marketList
+  }
 };
 </script>
 <style lang="scss" scoped>
-.mint-navbar .mint-tab-item.is-selected{
-  border-bottom-color:#00c17b;
-  color:#00c17b;
+.mint-navbar .mint-tab-item.is-selected {
+  border-bottom-color: #00c17b;
+  color: #00c17b;
 }
-.mint-navbar .mint-tab-item{
-  padding:0.5rem 0;
+.mint-navbar .mint-tab-item {
+  padding: 0.5rem 0;
+  &:first-child {
+    border-right: 1px solid #ddd;
+  }
 }
-/deep/ .mint-tab-item-label{
-  font-size:1.2rem;
+/deep/ .mint-tab-item-label {
+  font-size: 1.2rem;
 }
-.mint-navbar{
-  margin-top:2.75rem;
+.mint-navbar {
+  margin-top: 2.75rem;
 }
-.mint-tab-container{
-  margin-top:2rem;
+.mint-tab-container {
+  margin-top: 2rem;
 }
 </style>
